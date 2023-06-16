10ba2aac2fbf20a48db4e9f553546a55c745128d
fabi <luisxd0812@gmail.com>
2023-06-16 02:22:28 -0500

    Creacion del proyecto en NET. Core MVC - Sobre un Ecommers - Tienda Online

---
+++ Scripts/insert_onlineStore.sql
@@ -0,0 +1,20 @@
+
+USE BD_Ecommers_Store
+go
+
+
+
+insert into Persona(NombreCompleto,Correo,Clave,Rol) values
+('administrador','admin@example.com','123','Usuario')
+go
+
+insert into Categoria(Nombre) values
+('Caballeros'),
+('Damas'),
+('Infantes'),
+('Deportivos'),
+('Edicion Limitada'),
+('Smart')
+GO
+
+
