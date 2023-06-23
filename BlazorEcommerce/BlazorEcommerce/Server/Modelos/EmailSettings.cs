14fb7462f921e97b295197186c11bf5c0c6d7239
Jhoset Llacchua <jhosetsales@gmail.com>
2023-06-23 14:38:32 -0500

    Se implemento correo de bienvenida cuando se registra un nuevo usuario

---
+++ BlazorEcommerce/BlazorEcommerce/Server/Modelos/EmailSettings.cs
@@ -0,0 +1,12 @@
+﻿namespace BlazorEcommerce.Server.Modelos
+{
+    public class EmailSettings
+    {
+
+        public string SmtpServer { get; set; } = string.Empty;
+        public int SmtpPort { get; set; }
+        public string Username { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+
+    }
+}
